26156755c9622ff59319498c168d430e451ab5de
Ronny <ronny761@hityash.org>
2021-12-26 15:52:36 +0200

    edit screen

--- app/src/main/java/com/example/firebaseexample2021/EditPost_Screen.java
+++ app/src/main/java/com/example/firebaseexample2021/EditPost_Screen.java
@@ -1,13 +1,28 @@
 package com.example.firebaseexample2021;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class EditPost_Screen extends AppCompatActivity {
+public class EditPost_Screen extends AppCompatActivity implements View.OnClickListener {
     EditText et_title, et_body;
     Button btn_save;
+
+    String key;
+    Post post;
+
+    DatabaseReference postRef;
 
 
     @Override
@@ -15,6 +30,44 @@ public class EditPost_Screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_post_screen);
 
-        et_body = findViewById(R.id.e)
+        et_body = findViewById(R.id.etBody_edit);
+        et_title = findViewById(R.id.etTitle_edit);
+        btn_save = findViewById(R.id.btnSave_edit);
+        btn_save.setOnClickListener(this);
+
+        Intent intent = getIntent();
+        key = intent.getExtras().getString("key");
+        retrieveData();
+    }
+
+    private void retrieveData() {
+        postRef = FirebaseDatabase.getInstance().getReference("Posts/" + key);
+        postRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                post = snapshot.getValue(Post.class);
+                et_body.setText(post.body);
+                et_title.setText(post.title);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btn_save){
+            postRef = FirebaseDatabase.getInstance().getReference("Posts/" + key);
+            post.uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            post.title  = et_title.getText().toString();
+            post.body = et_body.getText().toString();
+            postRef.setValue(post);
+            finish();
+        }
     }
 }
